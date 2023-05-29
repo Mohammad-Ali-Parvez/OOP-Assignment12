caca80784e241d41b638512608371570fb37f0de
Mohammad Ali Parvez <mohammadaparvez@outlook.com>
2023-05-30 01:04:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OOP_Assignment12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\USER\Downloads\JSON\myTeam.json";
+            List<members> myteam = new List<members>();
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                var jsonString = streamReader.ReadToEnd();
+                myteam = JsonConvert.DeserializeObject<List<members>>(jsonString);
+            }
+            foreach (var item in myteam)
+            {
+                Console.WriteLine(
+                    "Name:{0},Date Of Birth:{1},Email:{2},Mobile:{3}", 
+                    item.Name, item.DateOfBirth, item.ContactInformation[0].Email, item.ContactInformation[0].Mobile);
+            }
+
+        }
+    }
+}
---
+++ myTeam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment12
+
+    {
+
+        public class contact
+        {
+            public contact(string email, string mobile)
+            {
+                Email = email;
+                Mobile = mobile;
+            }
+
+            public string Email { get; set; }
+            public string Mobile { get; set; }
+
+            public override string ToString()
+            {
+                return "Email:" + Email + "phone" + Mobile;
+            }
+
+        }
+        public class members
+        {
+            public string Name { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public List<contact> ContactInformation { get; set; }
+        }
+
+    }
